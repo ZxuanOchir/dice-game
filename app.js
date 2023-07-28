dfe2b07190b3e43631dbf73de13f59e1ce12371f
OCh <zxuan763@gmail.com>
2023-07-28 15:46:39 +0800

    toglogcin eeljig soliloo

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 var diceNumber = Math.floor(Math.random() * 6) + 1 ; 
 
-document.querySelector('#score-1').textContent = diceNumber;
-document.querySelector('#score-0').textContent = diceNumber;
+// document.querySelector('#score-1').textContent = diceNumber;
+// document.querySelector('#score-0').textContent = diceNumber;
 // document.querySelector('#score-1').innerHTML = "yes!"
 //Программ эхлэхэд бэлтгэе.
 
